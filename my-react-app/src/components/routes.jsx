cecaf1760d7193a90b3de3f3e293dcb73d1e3522
Alvaro <alvaromar988@gmail.com>
2024-06-19 13:32:34 +0200

    cambios en routes.jsx añadir import

--- my-react-app/src/components/routes.jsx
+++ my-react-app/src/components/routes.jsx
@@ -1,1 +1,31 @@
- 
+// src/routes.js
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Pages/Home.js";
+import Profile from "./components/Pages/Profile";
+import Properties from "./components/Pages/Properties";
+import Reservations from "./components/Pages/Reservations";
+import Incidents from "./components/Pages/Incidents";
+import Help from "./components/Pages/Help";
+import Chat from "./components/Pages/Chat";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/incidents" element={<Incidents />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
