d172a82521f1d2e785c0ed6ea347418347d33e7b
Alvaro <alvaromar988@gmail.com>
2024-06-28 15:48:43 +0200

    alvaro cambios

--- my-react-app/src/firebaseConfig.js
+++ my-react-app/src/firebaseConfig.js
@@ -1,27 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics, isSupported } from 'firebase/analytics'; // Importa isSupported para comprobar el entorno
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics"; // Importa isSupported para comprobar el entorno
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Configuración de tu aplicación web en Firebase usando variables de entorno
-const firebaseConfig = {
-  apiKey: "AIzaSyANAjJ8qVI0gEvE90TxcV4fahf1BdrLN2Y",
-  authDomain: "software-gloove.firebaseapp.com",
-  projectId: "software-gloove",
-  storageBucket: "software-gloove.appspot.com",
-  messagingSenderId: "322049603815",
-  appId: "1:322049603815:web:60ebde0679e0d847224a42",
-  measurementId: "G-KFYP5CEEWG"
-};
+const firebaseConfig = {};
 
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 let analytics;
-if (typeof window !== 'undefined' && isSupported()) {
+if (typeof window !== "undefined" && isSupported()) {
   analytics = getAnalytics(app);
 }
 const auth = getAuth(app);
@@ -29,4 +18,3 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { firebaseConfig, auth, db, storage, analytics };
-
--- my-react-app/testEnv.js
+++ my-react-app/testEnv.js
@@ -1,5 +1,1 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-console.log(process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'));
+ 
